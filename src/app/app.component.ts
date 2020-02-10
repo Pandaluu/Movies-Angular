1253457f2173f7559bfdd5e0697a6aa2feb43151
Pandaluu <luciie.30@live.fr>
2020-02-10 14:28:28 +0100

    add list movies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'movies';
+  public title = 'movies';
+
+  public defaultCountry = 'us';
+
+  public movies: any[] = [
+    {
+      title: 'Joker',
+      year: 2019,
+      country: 'us',
+      shown: true
+    },
+    {
+      title: 'Avengers',
+      year: 2012,
+      country: 'us',
+      shown: true
+    },
+    {
+      title: 'Interstellar',
+      year: 2014,
+      country: 'br',
+      shown: true
+
+    }
+  ];
+
+  public toggleCountry(): void {
+    this.defaultCountry =
+    (this.defaultCountry === 'us') ? this.defaultCountry = 'br'
+                                  : this.defaultCountry = 'us';
+
+    this.movies.forEach((movie: any) => {
+      movie.shown = (movie.country === this.defaultCountry);
+    });
+  }
 }
